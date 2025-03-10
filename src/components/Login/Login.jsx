185d64ce79d0ff2568f9771c8b0c4e66f283a244
Dasha-Vin <dashavinaeva98gmail.com@mail.ru>
2025-03-10 17:13:17 +0500

    first commit

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,72 +1,75 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { db } from '../../firebaseConfig'; // Импорт конфигурации Firebase
-import { collection, getDocs, query, where } from "firebase/firestore"; // Импорт функций Firestore для работы с базой данных
 import './Login.css'; // Импорт стилей
+import Back from '../../assets/back.png';
 
 const Login = ({ setUserId }) => {
-    // Состояния для хранения введенного логина, пароля и возможных ошибок
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [error, setError] = useState('');
+    const [email, setEmail] = useState('');  // Состояние для хранения email
+    const [password, setPassword] = useState('');  // Состояние для хранения пароля
+    const [error, setError] = useState('');  // Состояние для хранения ошибки
 
-    const navigate = useNavigate(); // Хук для навигации по страницам
+    const navigate = useNavigate();  // Хук для навигации
 
-    // Функция обработки входа в систему
-    const handleLogin = async (e) => {
-        e.preventDefault(); // Предотвращаем перезагрузку страницы при отправке формы
+    const handleEmail = (e) => {
+        e.preventDefault();  // Предотвращаем перезагрузку страницы при отправке формы
         
-        try {
-            const employeesRef = collection(db, "employees"); // Получаем коллекцию сотрудников
-            // Создаем запрос для поиска пользователя по введенному логину и паролю
-            const q = query(employeesRef, where("login", "==", email), where("password", "==", password));
-            const querySnapshot = await getDocs(q); // Выполняем запрос к базе данных
-            
-            if (!querySnapshot.empty) { // Если найдены документы в коллекции (пользователь существует)
-                const userDoc = querySnapshot.docs[0]; // Берем первый найденный документ
-                setUserId(userDoc.id); // Сохраняем ID пользователя (чтобы использовать в профиле)
-                navigate('/profile'); // Перенаправляем пользователя на страницу профиля
-            } else {
-                setError('Неверные логин или пароль'); // Выводим сообщение об ошибке
-            }
-        } catch (err) {
-            console.error("Ошибка при входе: ", err);
-            setError('Произошла ошибка'); // Выводим сообщение об ошибке, если что-то пошло не так
+        // Симулированные данные пользователей
+        const simulatedUsers = [
+            { id: '1', email: 'user@example.com', password: 'password123' },
+            { id: '2', email: 'admin@example.com', password: 'admin123' }
+        ];
+
+        // Проверяем, соответствует ли введенный email и пароль одному из симулированных пользователей
+        const user = simulatedUsers.find(user => user.email === email && user.password === password);
+
+        if (user) {
+            setUserId(user.id); // Сохраняем идентификатор пользователя
+            navigate('/profile'); // Перенаправление на страницу профиля
+        } else {
+            setError('Неверные почта или пароль'); // Устанавливаем сообщение об ошибке
         }
     };
 
+    const handleBackClick = () => {
+        navigate('/'); 
+    };
+
     return (
-        <div className="LoginContainer">
-            <form onSubmit={handleLogin}> {/* Форма входа */}
-                <h2 className='OpenLog'>Вход</h2>
-                <div className="form_item">
-                    {/* Поле ввода логина */}
-                    <input 
-                        type="email" 
-                        className="form__name form" 
-                        name="email" 
-                        placeholder="Логин" 
-                        value={email} 
-                        onChange={(e) => setEmail(e.target.value)} // Обновляем состояние email при изменении ввода
-                        required 
-                    />
-                </div>
-                <div className="form_item">
-                    {/* Поле ввода пароля */}
-                    <input 
-                        type="password" 
-                        className="form__name form" 
-                        name="password" 
-                        placeholder="Пароль" 
-                        value={password} 
-                        onChange={(e) => setPassword(e.target.value)} // Обновляем состояние password при изменении ввода
-                        required 
-                    />
-                </div>
-                {/* Кнопка отправки формы (входа) */}
-                <button className='Open' type="submit">Войти</button>
-                {error && <p>{error}</p>} {/* Отображение ошибки, если она есть */}
-            </form>
+        <div>
+            <div className="Arrow-back" onClick={handleBackClick}>
+                <img className="backImage" src={Back} alt="Назад" />
+                <h3 className="back">Назад</h3>
+            </div>
+
+            <div className="LoginContainer">
+                <form onSubmit={handleEmail}>
+                    <h2 className='OpenLog'>Вход</h2>
+                    <div className="form_item">
+                        <input 
+                            type="email" 
+                            className="form__name form" 
+                            name="email" 
+                            placeholder="Адрес эл. почты" 
+                            value={email} 
+                            onChange={(e) => setEmail(e.target.value)} 
+                            required 
+                        />
+                    </div>
+                    <div className="form_item">
+                        <input 
+                            type="password" 
+                            className="form__name form" 
+                            name="password" 
+                            placeholder="Пароль" 
+                            value={password} 
+                            onChange={(e) => setPassword(e.target.value)} 
+                            required 
+                        />
+                    </div>
+                    <button className='Open' type="submit">Войти</button>
+                    {error && <p>{error}</p>} {/* Отображаем сообщение об ошибке, если оно есть */}
+                </form>
+            </div>
         </div>
     );
 };
