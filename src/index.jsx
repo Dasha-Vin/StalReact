5fe6e4ed5ab8ab27714f3fe1156e344cbf124c47
Dasha-Vin <dashavinaeva98gmail.com@mail.ru>
2025-03-10 17:43:18 +0500

    first commit

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './reset.css'
 import './index.css'
 import App from './App'
+import { DevTools } from 'react-devtools';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,3 +11,5 @@ root.render(
         <App />
     </React.StrictMode>
 );
+
+DevTools.register();
